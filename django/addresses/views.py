829e3e6ccfe16e868a683c11c9016f05242ce7bc
Jeffersson <barrosjefferson407@gmail.com>
2024-12-10 18:56:01 -0300

    Criação do ListAPI e DetailAPI de Address e City

--- django/addresses/views.py
+++ django/addresses/views.py
@@ -1,13 +1,33 @@
 from addresses.models import Address, City
 from addresses.serializers import AddressSerializer, CitySerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 
 class AddressViewSet(viewsets.ModelViewSet):
-    queryset = Address.objects.all()
+    queryset = Address.objects.all().order_by('id')
     serializer_class = AddressSerializer
 
 
 class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all().order_by('id')
+    serializer_class = CitySerializer
+
+
+class AddressListAPIView(generics.ListAPIView):
+    queryset = Address.objects.all().order_by('id')
+    serializer_class = AddressSerializer
+
+
+class CityListAPIView(generics.ListAPIView):
+    queryset = City.objects.all().order_by('id')
+    serializer_class = CitySerializer
+
+
+class AddressDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class CityDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = City.objects.all()
     serializer_class = CitySerializer
